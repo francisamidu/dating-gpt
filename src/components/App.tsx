f3e3c467016101294c89c98ada92d9d2de18966c
francisamidu <francisamidu124@gmail.com>
2023-02-06 18:46:12 +0200

    UI tweaks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <main className="w-full min-h-[90vh] min-w-[90vh]">
       <div className="flex flex-col items-center justify-center w-full h-full">
-        <h1 className="text-center">Welcome to Vite</h1>
+        <h1 className="text-center">Welcome to DatingGPT</h1>
       </div>
     </main>
   );
