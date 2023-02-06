4fee8d5a4235fc873ba46fd524b708bbf29a7802
francisamidu <francisamidu124@gmail.com>
2023-02-06 18:41:40 +0200

    UI tweaks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[90vh] min-w-[90vh]">
-      <h1 className="text-center">Welcome to Vite</h1>
-    </div>
+    <main className="w-full min-h-[90vh] min-w-[90vh]">
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <h1 className="text-center">Welcome to Vite</h1>
+      </div>
+    </main>
   );
 };
 
