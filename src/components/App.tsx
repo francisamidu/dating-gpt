df0eb4f711981baab24f276c40d7e94000db139a
francisamidu <francisamidu124@gmail.com>
2023-02-06 18:29:43 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[90vh] min-w-[90vh]">
+      <h1 className="text-center">Welcome to Vite</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  media: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
