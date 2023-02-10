ac3adade0b37766ea74733adbb38fc0172cb1f5f
francisamidu <francisamidu124@gmail.com>
2023-02-10 10:43:35 +0200

    UI tweaks

--- pages/advice.tsx
+++ pages/advice.tsx
@@ -1,11 +1,35 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { Layout } from "../components";
 
 const Advice = () => {
+  const [num, setNum] = useState(42005);
+  const [advice, setAdvice] = useState("");
+  const formatter = Intl.NumberFormat();
+  const text = `Helped ${formatter.format(num)} users so far `;
+
   return (
-    <section className="h-full md:max-w-[1100px] md:mx-auto my-4">
-      <h1 className="font-bold text-3xl mb-5 text-slate-800">Let's get you</h1>
-      <h2 className="text-gray-600">We here</h2>
+    <section className="h-full md:max-w-[1100px] md:mx-auto my-10">
+      <h1 className="font-bold text-4xl md:w-3/5 mb-3 text-slate-800">
+        Get any piece of relationship advice in seconds
+      </h1>
+      <h2 className="text-gray-500">
+        <span>{text}</span>
+      </h2>
+      <form className="w-3/5 mt-5">
+        <label htmlFor="advice" className="font-bold text-slate-500">
+          Please enter your query
+        </label>
+        <textarea
+          aria-label="advice"
+          value={advice}
+          onChange={(e) => setAdvice(e.target.value)}
+          rows={4}
+          className="w-full rounded-md border-2 border-gray-400 shadow-sm focus:border-black focus:ring-black my-5 py-2"
+          placeholder={
+            "e.g. What should I wear for a first date with a colleague?"
+          }
+        />
+      </form>
     </section>
   );
 };
