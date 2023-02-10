eea4b799149ecf79cc796838eba23f904d74de72
francisamidu <francisamidu124@gmail.com>
2023-02-10 10:23:43 +0200

    UI tweaks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import { Twitter, Github } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="p-4 w-full bg-white fixed bottom-0 left-0">
-      <div className="max-w-screen-lg md:mx-auto  flex flex-row items-center justify-between">
+    <footer className="w-full bg-white fixed bottom-0 left-0">
+      <div className="max-w-screen-lg md:mx-auto  flex flex-row items-center justify-between border-t border-t-gray-200 py-6">
         <p>
           Powered by{" "}
           <a href="https://openai.com" className="font-bold">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,9 @@ import { Github } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full px-2 py-2 sm:px-8 bg-white shadow">
-      <div className="mx-auto flex flex-row flex-wrap items-center justify-between md:max-w-[1100px]">
-        <a href="#" className="self-center whitespace-nowrap text-md font-bold">
+    <nav className="w-full sm:px-8 bg-white">
+      <div className="mx-auto flex flex-row flex-wrap items-center justify-between md:max-w-[1100px] border-b border-b-200 py-4">
+        <a href="/" className="self-center whitespace-nowrap text-xl font-bold">
           {shared.name}
         </a>
         <Button
