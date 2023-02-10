b272e1eb3cd3c9d87eb9847631bf5b2579b18cf6
francisamidu <francisamidu124@gmail.com>
2023-02-10 10:26:00 +0200

    UI tweaks

--- pages/advice.tsx
+++ pages/advice.tsx
@@ -3,8 +3,8 @@ import { Layout } from "../components";
 
 const Advice = () => {
   return (
-    <section className="h-full">
-      <h1 className="font-bold text-3xl my-5">Let's get you</h1>
+    <section className="h-full md:max-w-[1100px] md:mx-auto my-4">
+      <h1 className="font-bold text-3xl mb-5 text-slate-800">Let's get you</h1>
       <h2 className="text-gray-600">We here</h2>
     </section>
   );
