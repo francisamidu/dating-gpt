5b8d7c2ed4bb521ec92bee4bb3b0f274fac17f0c
francisamidu <francisamidu124@gmail.com>
2023-02-10 10:47:53 +0200

    UI tweaks

--- pages/advice.tsx
+++ pages/advice.tsx
@@ -8,15 +8,17 @@ const Advice = () => {
   const text = `Helped ${formatter.format(num)} users so far `;
 
   return (
-    <section className="h-full md:max-w-[1100px] md:mx-auto my-10">
-      <h1 className="font-bold text-4xl md:w-3/5 mb-3 text-slate-800">
-        Get any piece of relationship advice in seconds
-      </h1>
-      <h2 className="text-gray-500">
-        <span>{text}</span>
-      </h2>
-      <form className="w-3/5 mt-5">
-        <label htmlFor="advice" className="font-bold text-slate-500">
+    <section className="h-full md:max-w-[1100px] md:mx-auto my-10 flex flex-col item-center">
+      <div className="text-center md:w-3/5">
+        <h1 className="font-bold text-4xl mb-3 text-slate-700">
+          Get any piece of relationship advice in seconds
+        </h1>
+        <h2 className="text-gray-500">
+          <span>{text}</span>
+        </h2>
+      </div>
+      <form className="w-3/5 mt-10">
+        <label htmlFor="advice" className="font-bold text-slate-700">
           Please enter your query
         </label>
         <textarea
@@ -24,7 +26,7 @@ const Advice = () => {
           value={advice}
           onChange={(e) => setAdvice(e.target.value)}
           rows={4}
-          className="w-full rounded-md border-2 border-gray-400 shadow-sm focus:border-black focus:ring-black my-5 py-2"
+          className="w-full rounded-md border-2 border-slate-200 shadow-sm focus:border-slate-500 focus:ring-slate-500 p-2 mt-3"
           placeholder={
             "e.g. What should I wear for a first date with a colleague?"
           }
